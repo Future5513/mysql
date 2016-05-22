fdca3bb1c3900d8e7ab57d461eb1a6bb5bf58cb1
Future5513 <future@futuredemacbook-air.local>
2016-05-22 23:50:58 +0800

    Initial Commit

---
+++ mysql/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mysql
+//
+//  Created by future on 16/5/22.
+//  Copyright © 2016年 future. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
